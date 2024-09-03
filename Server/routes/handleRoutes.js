36deed50630f97a477721ea4f98e5cb51762f2f2
Kamesh Chaudhary <kameshchaudhary17@gmail.com>
2024-09-03 21:49:24 +0545

    add resorce

--- Server/routes/handleRoutes.js
+++ Server/routes/handleRoutes.js
@@ -1,15 +1,20 @@
 import express from 'express';
 import{authenticateUser, authorizeAdmin} from '../middleware/authToken.js';
 import {addResource, checkoutResource, getResources, returnResource} from '../controller/resourceController.js';
+import {multer, storage } from '../middleware/multerConfig.js';
+
+
+
+const upload = multer({storage : storage})
 
 
 const handleRoutes = express.Router();
 
 // Public route (e.g., to get all resources)
-handleRoutes.get('/', authenticateUser, getResources);
+handleRoutes.get('/resources', authenticateUser, getResources);
 
 // Admin routes
-handleRoutes.post('/resource', authenticateUser, authorizeAdmin, addResource);
+handleRoutes.post('/resource', authenticateUser, upload.single('photo'), authorizeAdmin, addResource);
 handleRoutes.post('/:id/checkout', authenticateUser, authorizeAdmin, checkoutResource);
 handleRoutes.post('/:id/return', authenticateUser, authorizeAdmin, returnResource);
 
